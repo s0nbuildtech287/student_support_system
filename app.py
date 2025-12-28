fe382a59144432dce7bebdcd3675e54831647202
Xuan Son <buixu4ns0n@gmail.com>
2025-12-29 01:45:20 +0700

    ranking

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect
 from routes.subject import subject_bp
 from routes.home import home_bp
-# from routes.ranking import ranking_bp  
+from routes.ranking import ranking_bp
 from routes.user import user_bp
 from routes.roadmap import roadmap_bp
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Đăng ký route (Blueprint)
 app.register_blueprint(home_bp)
 app.register_blueprint(subject_bp)
-# app.register_blueprint(ranking_bp)       
+app.register_blueprint(ranking_bp)       
 app.register_blueprint(user_bp)
 app.register_blueprint(roadmap_bp)
 
